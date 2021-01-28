dd8d143f65effbe5466383f5e5d6ce0344d669fc
nicoWeb31 <nico.riot@free.fr>
2021-01-28 17:10:02 +0100

    grid sur home

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -22,73 +22,67 @@ const HomePage = () => {
                     {" "}
                     Développeur Web/Web Mobile Mern Stack
                 </TitreSecondary>
-                <div className="row">
-                    <div className="col-md-6 col-12">
-                        <div className="presentation ">
-                            <figure className="presentation__shape">
-                                <img
-                                    src={me}
-                                    alt="riot nicolas"
-                                    className="presentation__img"
-                                />
-                            </figure>
-                            <div className="presentation__text">
-                                <h3 className="heading-tertiary u-margin-bottom-small">
-                                    Qui suis-je ?
-                                </h3>
-                                <p>
-                                    Bonjour, je m'appelle Nicolas Riot. Je suis
-                                    en reconvertion professionnelle depuis
-                                    maintenant 3 ans. Suite à un bilan de
-                                    compétences je me suis lancé dans une
-                                    licence en distanciel de
-                                    développeur-concepteur d'application web.
-                                    Après l'apprentissage des bases du web
-                                    (html5, css3) et du php (+poo), j'ai réalisé
-                                    mon premier projet important sous Symfony4,
-                                    projet qui m'a servit de support à la
-                                    soutenance de ma licence. Depuis l'obtention
-                                    de mon diplôme, je monte en compétence sur
-                                    React et Node; Javascript étant un langage
-                                    que j'adore utiliser et que je trouve
-                                    tellement puissant.
-                                </p>
-                            </div>
+                <div className="rowGrid">
+                    <div className="presentation perso">
+                        <figure className="presentation__shape">
+                            <img
+                                src={me}
+                                alt="riot nicolas"
+                                className="presentation__img"
+                            />
+                        </figure>
+                        <div className="presentation__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">
+                                Qui suis-je ?
+                            </h3>
+                            <p>
+                                Bonjour, je m'appelle Nicolas Riot. Je suis en
+                                reconvertion professionnelle depuis maintenant 3
+                                ans. Suite à un bilan de compétences je me suis
+                                lancé dans une licence en distanciel de
+                                développeur-concepteur d'application web. Après
+                                l'apprentissage des bases du web (html5, css3)
+                                et du php (+poo), j'ai réalisé mon premier
+                                projet important sous Symfony4, projet qui m'a
+                                servit de support à la soutenance de ma licence.
+                                Depuis l'obtention de mon diplôme, je monte en
+                                compétence sur React et Node; Javascript étant
+                                un langage que j'adore utiliser et que je trouve
+                                tellement puissant.
+                            </p>
                         </div>
                     </div>
-                    <div className="col-md-6 col-12">
-                        <div className="presentation ">
-                            <figure className="presentation__shape">
-                                <img
-                                    src={code}
-                                    alt="code"
-                                    className="presentation__img"
-                                />
-                            </figure>
-                            <div className="presentation__text">
-                                <h3 className="heading-tertiary u-margin-bottom-small">
-                                    Pourquoi l'UX ? Pourquoi en alternance ?
-                                </h3>
-                                <p>
-                                    Suite à l'obtention de ma licence et fort de
-                                    mon expérience de travail en distanciel, je
-                                    me suis dis : Pourquoi ne pas continuer sur
-                                    ma lancée ? Ayant survolé l'UX design et
-                                    voulant vraiment en apprendre plus sur ce
-                                    domaine, j'ai décidé de postuler chez
-                                    OpenClassroom pour le master d'UX designer.
-                                    Aprés réflexion et souhaitant par la même
-                                    occasion engranger de l'expérience au sein
-                                    d'une entreprise, j'ai fait le choix de
-                                    l'alternance. Au rythme proposé par
-                                    OpenClassroom de 4 jours en entreprise et 1
-                                    jour en formation en ligne, cela permet de
-                                    maintenir un projet sur la continuité au fil
-                                    des semaines, sans interruption. De plus
-                                    cette formation est composée à 100% par des
-                                    projets concrets, quoi de plus formateur ?
-                                </p>
-                            </div>
+
+                    <div className="presentation ux">
+                        <figure className="presentation__shape">
+                            <img
+                                src={code}
+                                alt="code"
+                                className="presentation__img"
+                            />
+                        </figure>
+                        <div className="presentation__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">
+                                Pourquoi l'UX ? Pourquoi en alternance ?
+                            </h3>
+                            <p>
+                                Suite à l'obtention de ma licence et fort de mon
+                                expérience de travail en distanciel, je me suis
+                                dis : Pourquoi ne pas continuer sur ma lancée ?
+                                Ayant survolé l'UX design et voulant vraiment en
+                                apprendre plus sur ce domaine, j'ai décidé de
+                                postuler chez OpenClassroom pour le master d'UX
+                                designer. Aprés réflexion et souhaitant par la
+                                même occasion engranger de l'expérience au sein
+                                d'une entreprise, j'ai fait le choix de
+                                l'alternance. Au rythme proposé par
+                                OpenClassroom de 4 jours en entreprise et 1 jour
+                                en formation en ligne, cela permet de maintenir
+                                un projet sur la continuité au fil des semaines,
+                                sans interruption. De plus cette formation est
+                                composée à 100% par des projets concrets, quoi
+                                de plus formateur ?
+                            </p>
                         </div>
                     </div>
                 </div>
