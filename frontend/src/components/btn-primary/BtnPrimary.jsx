20452cb17273532c35dcd20b172c06d96d8ea8d9
nicoWeb31 <nico.riot@free.fr>
2021-02-03 12:20:38 +0100

    refact btn

--- frontend/src/components/btn-primary/BtnPrimary.jsx
+++ frontend/src/components/btn-primary/BtnPrimary.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
-
+import './btnPrimary.style.scss'
 
 const BtnPrimary = ({children, linkTo}) => {
     return (
         <Link to={linkTo} className="btn btn-white btn-animated">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         {" "}
         {children}
     </Link>
