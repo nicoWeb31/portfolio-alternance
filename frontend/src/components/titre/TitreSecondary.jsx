9ca95d544528196419d717893a9f1c42013b3470
nicoWeb31 <nico.riot@free.fr>
2021-02-03 14:15:11 +0100

    test css

--- frontend/src/components/titre/TitreSecondary.jsx
+++ frontend/src/components/titre/TitreSecondary.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import './titreSecondary.style.scss';
+import React from "react";
+import "./titreSecondary.style.scss";
+import { useSpring, animated } from "react-spring";
 
-const TitreSecondary = ({children}) => {
+const TitreSecondary = ({ children }) => {
+    const props = useSpring({
+        to: async (next, cancel) => {
+            await next({ opacity: 0 });
+            await next({ opacity: 0.5 });
+            await next({ opacity: 0.3 });
+            await next({ opacity: 1 });
+        },
+        from: { opacity: 0 },
+        config: { duration: 1000 },
+    });
+
     return (
-        <div className="u-center-text">
-            <h2 className="heading-secondary">
-            {children}
-            </h2>
+        <animated.div className="u-center-text" style={props}>
+            <h2 className="heading-secondary">{children}</h2>
+        </animated.div>
+    );
+};
 
-        </div>
-    )
-}
-
-export default TitreSecondary
+export default TitreSecondary;
